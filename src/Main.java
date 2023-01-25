8957cc6c9c6fe40859d114e12c92d7596a7145d6
JungSoyi <soi0125@naver.com>
2023-01-25 17:24:56 +0900

    :sparkles: 양궁 문제

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
-import employee.Employee;
+import player.Player;
 
 public class Main {
     public static void main(String[] args){
 
-        String[] name = {"직원1", "직원2", "직원3"};
-        int[][] hours = {{30,21,43,36,33},{30,21,43,36,33},{30,21,43,36,33}};
+        /**
+         * employee 문제
+         */
+//        String[] name = {"직원1", "직원2", "직원3"};
+//        int[][] hours = {{30,21,43,36,33},{30,21,43,36,33},{30,21,43,36,33}};
+//
+//        Employee[] employees = new Employee[name.length];
+//
+//        for(int i=0; i<name.length;i++){
+//            employees[i] = new Employee(name[i],hours[i]);
+//            employees[i].printTotalHours();
+//            employees[i].totalHours();
+//        }
 
-        Employee[] employees = new Employee[name.length];
-
+        /**
+         * 양궁 문제
+         */
+        String[] name = {"Kim", "Lee", "Park"};
+        int[][] points = {{10,9,9,8},{9,10,9,9},{10,9,10,10}};
         for(int i=0; i<name.length;i++){
-            employees[i] = new Employee(name[i],hours[i]);
-            employees[i].printTotalHours();
-            employees[i].totalHours();
+            Player player = new Player(name[i],points[i]);
+            player.score();
         }
 
     }
---
+++ src/player/Player.java
@@ -0,0 +1,25 @@
+package player;
+
+public class Player {
+    // 1. 필드 정의
+    static String name; //선수 이름
+    static int[] points; //득점 상황
+
+    // 2. 생성자 만들기
+    public Player(String name, int[] points){
+        this.name = name;
+        this.points = points;
+    }
+
+    // 3. 메서드 정의: 선수 별 총 점 계산하기
+    public static void score(){
+        int total_score=0;
+        for(int i=0; i<points.length; i++){
+            total_score += points[i];
+            System.out.printf("선수 %s의 총 점: %d \n", name, total_score);
+        }
+
+
+    }
+
+}
